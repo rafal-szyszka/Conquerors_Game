214f4491f3c49de334ffe0a7169712ecb119021e
Rafał Szyszka <rafal@szyszka.it>
2018-01-27 14:55:43 +0100

    Added sidebar menu

---
+++ src/components/menu/side/MainSideMenu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container-fluid" style="position: absolute; top: 90px; z-index: -1;">
+    <div class="row">
+      <div class="col-sm-2 cnq-menu-bar">
+        <img src="../../../assets/cnqrrs.png" width="200px"/>
+      </div>
+    </div>
+    <menu-entry
+      v-for="menuEntry in menuEntries"
+      :menu-element="menuEntry"
+    />
+  </div>
+</template>
+
+<script>
+  import MenuEntry from "./MenuEntry";
+
+  export default {
+    components: {MenuEntry},
+    name: "main-side-menu",
+    data: function () {
+      return {
+        menuEntries: [
+          {name: "Zadania", isModalDialog: true, imgSrc: "./src/assets/menu-icons/menu-tasks.png"},
+          {name: "Depesze", isModalDialog: true, imgSrc: "./src/assets/menu-icons/menu-reports.png"},
+          {name: "Wiadomości", isModalDialog: true, imgSrc: "./src/assets/menu-icons/menu-messages.png"},
+          {name: "Ranking", isModalDialog: true, imgSrc: "./src/assets/menu-icons/menu-rank.png"},
+          {name: "Pomoc", isModalDialog: true, imgSrc: "./src/assets/menu-icons/menu-help.png"},
+          {name: "Wyloguj", isModalDialog: false}
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .cnq-menu-bar {
+    background-color: burlywood;
+    border-top-right-radius: 75px;
+    padding: 10px 10px 20px 20px;
+  }
+
+</style>
---
+++ src/components/menu/side/MenuEntry.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="row">
+    <div class="col-sm-2 cnq-menu-entry" v-if="menuElement.isModalDialog">
+      <button class="btn btn-menu-entry">
+        <img :src="menuElement.imgSrc"/>
+        {{ menuElement.name }}
+      </button>
+    </div>
+    <div class="col-sm-2 cnq-menu-entry last-entry" v-else>
+      <button class="btn btn-menu-entry">
+        <img :src="menuElement.imgSrc"/>
+        {{ menuElement.name }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "menu-entry",
+    props: ['menuElement', 'totalElements', 'currentElement']
+  }
+</script>
+
+<style scoped>
+
+  @import url('https://fonts.googleapis.com/css?family=Quicksand');
+
+  .btn-menu-entry {
+    background-color: #754237;
+    border: 1px solid saddlebrown;
+    color: whitesmoke;
+    font-family: "Quicksand", sans-serif;
+    font-weight: 600;
+    width: inherit;
+  }
+
+  .cnq-menu-entry {
+    background-color: burlywood;
+    border-top: 1px solid black;
+    padding: 15px 10px 15px 15px;
+    text-align: center;
+  }
+
+  .last-entry {
+    background-color: burlywood;
+    border-top: 1px solid black;
+    border-bottom-right-radius: 15px;
+    padding: 15px 10px 15px 15px;
+    text-align: center;
+
+  }
+
+</style>
